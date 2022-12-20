584f158984b683b7c7c6ea1f720bd690cdcf9cea
Sergii Kukhariev <serge.kukharev@gmail.com>
2022-12-20 12:01:02 -0300

    Users can disable email tracking across their account

--- src/User.test.ts
+++ src/User.test.ts
@@ -10,4 +10,16 @@ describe('User class', () => {
         expect(() => new User(" ").id).toThrowError("You should provide User Id");
         expect(() => new User(`\t`).id).toThrowError("You should provide User Id");
     })
+
+    it('has a setting for email tracking and by default it is enabled', () => {
+        expect(new User("abcdef").isTrackingOfEmailsEnabled).toBe(true);
+    });
+
+    it ('can disable email tracking across its account', () => {
+        const user = new User("abcdef");
+
+        user.disableEmailTracking();
+
+        expect(user.isTrackingOfEmailsEnabled).toBe(false);
+    })
 });
--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,6 @@
 class User {
     readonly _id: string;
-
+    private _isTrackingOfEmailsEnabled = true;
     constructor(id: string) {
         if (id.trim().length === 0) throw new Error("You should provide User Id");
         this._id = id;
@@ -8,6 +8,17 @@ class User {
 
     get id(): string {
         return this._id;
+    }
+
+    get isTrackingOfEmailsEnabled(): boolean {
+        return this._isTrackingOfEmailsEnabled;
+    }
+
+    disableEmailTracking() {
+        // Maybe this should cause some Domain Event that will be consumed by Sequence service and update settings
+        // of all sequences that belong to this user. Or maybe not. At least we have control over it.
+
+        this._isTrackingOfEmailsEnabled = false;
     }
 }
 
